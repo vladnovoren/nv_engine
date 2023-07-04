9ba15f6098628e89b8671833975494462a008720
novikov vladislav <novikov.vm@phystech.edu>
2023-07-04 22:22:38 +0500

    added transform & perspective camera

--- src/Tests/Renderer/TestVAO/TestVAO.cpp
+++ src/Tests/Renderer/TestVAO/TestVAO.cpp
@@ -1,6 +1,9 @@
 #include "Buffers/Types.hpp"
 #include "Renderer_OpenGL.hpp"
+#include <glm/ext/matrix_transform.hpp>
+#include <glm/geometric.hpp>
 #include <iostream>
+#include "Core/Camera/PerspectiveCamera.hpp"
 
 struct Vertex {
   glm::vec3 pos;
@@ -15,19 +18,13 @@ int main() {
   context.SwitchTo();
 
   std::vector<Vertex> vertices{
-    {{-1, 0, 0}, {1, 0, 0}},
-    {{-0.5, 0.5, 0}, {0, 1, 0}},
-    {{0, 0, 0}, {0, 0, 1}},
-
-    {{0, 0, 0}, {1, 0, 0}},
-    {{0.5, 0.5, 0}, {0, 1, 0}},
-    {{1, 0, 0}, {0, 0, 1}}
+    {{-10, 0, 0}, {1, 0, 0}},
+    {{0, 10, 0}, {0, 1, 0}},
+    {{10, 0, 0}, {0, 0, 1}}
   };
 
   std::vector<unsigned int> indices{
-    0, 2, 1,
-
-    3, 5, 4
+    0, 2, 1
   };
 
   gl::Program program("../src/Tests/Renderer/TestVAO/Vert.vert",
@@ -51,7 +48,34 @@ int main() {
   vao.AddVertexBuffer(vbo);
   vao.SetIndexBuffer(ebo);
 
+  PerspectiveCamera camera;
+  camera.transform.position = glm::vec3(0, 0, 100.f);
+  //camera.transform.Rotate(glm::radians(180.0f), kUp);
+  auto forward = camera.transform.CalculateForward();
+
+  std::cout << forward.x << ' ' << forward.y << ' ' << forward.z << '\n';;
+
+  float ds = 1;
+
   while (!window.ShouldClose() && !window.IsKeyDown(gl::eKey::ESCAPE)) {
+    if (window.IsKeyDown(gl::eKey::W)) {
+      camera.transform.position += camera.transform.CalculateForward() * ds;
+    }
+    if (window.IsKeyDown(gl::eKey::S)) {
+      camera.transform.position -= camera.transform.CalculateForward() * ds;
+    }
+    if (window.IsKeyDown(gl::eKey::A)) {
+      camera.transform.position -= camera.transform.CalculateRight() * ds;
+    }
+    if (window.IsKeyDown(gl::eKey::D)) {
+      camera.transform.position += camera.transform.CalculateRight() * ds;
+    }
+
+    glm::mat4 mvp = camera.CalculatePerspectiveMatrix() *
+                    camera.CalculateViewMatrix();
+
+    program.SetUniform("mvp", mvp);
+
     context.PollEvents();
     context.Clear();
     context.DrawIndexed(vao, gl::ePrimitive::TRIANGLES, 0, vertices.size());
--- src/Tests/Renderer/TestVAO/Vert.vert
+++ src/Tests/Renderer/TestVAO/Vert.vert
@@ -3,11 +3,12 @@
 layout (location = 0) in vec3 in_position;
 layout (location = 1) in vec3 in_color;
 
+uniform mat4 mvp;
+
 out vec3 color;
 
 void main() {
-  gl_Position.xyz = in_position;
-  gl_Position.w = 1.0;
+  gl_Position = mvp * vec4(in_position, 1.0f);
 
   color = in_color;
 }
